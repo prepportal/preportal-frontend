4814f760fdd88653189f087e7fa36677b8995235
Akash R Chandran <chandranrakash@gmail.com>
2023-08-18 11:47:18 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import prepPortalLogo from './assets/logo.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={prepPortalLogo} className="logo" alt="Vite logo" width='500px' height='500px'/>
+        </a>
+      </div>
+      <h1>PrepPortal</h1>
+      <div className="card">
+        <p>
+        Elevating Learning, Enriching Futures.
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App

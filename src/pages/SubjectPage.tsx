3dbd64c64a330dd6f424765490d683a17031eab0
bhagyaunnikrishnan <bhagyanandhu2002@gmail.com>
2023-12-16 20:14:47 +0530

    changed

--- src/pages/SubjectPage.tsx
+++ src/pages/SubjectPage.tsx
@@ -1,6 +1,5 @@
 import {Table, TableContainer, Tbody, Td, Th, Thead, Tr, Button } from "@chakra-ui/react";
 
-
 const SubjectPage = () => {
  
   return (
@@ -8,44 +7,70 @@ const SubjectPage = () => {
       
 
       <TableContainer>
-        <Table variant='striped' colorScheme='purple' size='sm'>
+        <Table variant='unstyled' colorScheme='purple'>
           <Thead>
             <Tr>
-              <Th color='powderblue'>Subjects</Th>
-              <Th color='powderblue'>Link</Th>
+              <Th color='blueviolet' style={{ textAlign: 'center' }}>Subjects</Th>
             </Tr>
           </Thead>
           <Tbody>
             <Tr>
-              <Td>ENGINEERING MECHANICS (EST100)</Td>
-              <Td><Button variant='link' color='powderblue' fontWeight={"bold"}>Open</Button></Td>
+              <Td>
+                <Button variant='outline'  borderColor='purple' fontWeight={"bold"} w='full'>
+                  ENGINEERING MECHANICS (EST100)
+                </Button>
+              </Td>
             </Tr>
-            <Tr>
-              <Td>BASICS OF CIVIL AND MECHANICAL ENGINEERING (EST120)</Td>
-              <Td><Button variant='link' color='powderblue' fontWeight={"bold"}>Open</Button></Td>
-            </Tr>
-            <Tr>
-              <Td>ENGINEERING PHYSICS A (PHT100)</Td>
-              <Td><Button variant='link' color='powderblue' fontWeight={"bold"}>Open</Button></Td>
-            </Tr>
-            <Tr>
-              <Td>LINEAR ALGEBRA AND CALCULUS (MAT101)</Td>
-              <Td><Button variant='link' color='powderblue' fontWeight={"bold"}>Open</Button></Td>
-            </Tr>
-            <Tr>
-              <Td>LIFE SKILLS (HUN101)</Td>
-              <Td><Button variant='link' color='powderblue' fontWeight={"bold"}>Open</Button></Td>
-            </Tr>
-            <Tr>
-              <Td>BASICS OF ELECTRICAL AND ELECTRONICS ENGINEERING (EST130)</Td>
-              <Td><Button variant='link' color='powderblue' fontWeight={"bold"}>Open</Button></Td>
-            </Tr>
-            <Tr>
-              <Td>ENGINEERING GRAPHICS (EST110)</Td>
-              <Td><Button variant='link' color='powderblue' fontWeight={"bold"}>Open</Button></Td>
-            </Tr>
-          </Tbody>
 
+            <Tr>
+              <Td>
+                <Button variant='outline'  borderColor='purple' fontWeight={"bold"} w='full'>
+                  BASICS OF CIVIL AND MECHANICAL ENGINEERING (EST120)
+                </Button>
+              </Td> 
+            </Tr>
+
+            <Tr>
+              <Td>
+                <Button variant='outline'  borderColor='purple' fontWeight={"bold"} w='full'>
+                  ENGINEERING PHYSICS A (PHT100)
+                </Button>
+              </Td>
+            </Tr>
+
+            <Tr>
+              <Td>
+                <Button variant='outline' borderColor='purple' fontWeight={"bold"} w='full'>
+                  LINEAR ALGEBRA AND CALCULUS (MAT101)
+                </Button>
+              </Td> 
+            </Tr>
+
+            <Tr>
+              <Td>
+                <Button variant='outline' borderColor='purple' fontWeight={"bold"} w='full'>
+                  LIFE SKILLS (HUN101)
+                </Button>
+              </Td>
+            </Tr>
+
+            <Tr>
+              <Td>
+                <Button variant='outline'  borderColor='purple' fontWeight={"bold"} w='full'>
+                  BASICS OF ELECTRICAL AND ELECTRONICS ENGINEERING (EST130)
+                </Button>
+              </Td>
+            </Tr>
+
+            <Tr>
+              <Td>
+                <Button variant='outline'  borderColor='purple' fontWeight={"bold"} w='full'>
+                  ENGINEERING GRAPHICS (EST110)
+                </Button>
+              </Td>
+            </Tr>
+
+          </Tbody>
         </Table>
       </TableContainer>
     </>
